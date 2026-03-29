e96b5d74fcb91b2ec3c329d1be2ff200d7bacc43
Dustin Griffith <dustingriffith@outlook.com>
2026-03-29 10:35:47 -0400

    signin: added inputs

--- apps/demo/src/app/features/auth/signin/signin.component.ts
+++ apps/demo/src/app/features/auth/signin/signin.component.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [FormField, MatFormFieldModule, MatInputModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class Signin {}
+export default class Signin {
+  readonly #model = signal({
+    email: '',
+    password: '',
+  });
+
+  protected readonly form = form(this.#model);
+
+  protected onSubmit() {
+    console.log('Email:', this.#model().email);
+    console.log('Password:', this.#model().password);
+  }
+}
